36d06168b74b7dd66d8ee8b2237291687190b0f3
Cedric Dujardin <cedric.dujardin@laposte.net>
2019-12-15 23:46:50 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
   if board[index] == "X" || "O"
     true
-  elsif board[index] = " " || nil || ""
-      false
+  elsif board[index] == " " || "" || nil
+    false
   end
 end
