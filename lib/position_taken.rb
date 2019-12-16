94981e0a56603001321826d94765de4d17cee9c3
Cedric Dujardin <cedric.dujardin@laposte.net>
2019-12-16 00:54:57 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 # code your #position_taken? method here!
-def position_taken?(board, index)
-  if board[index] == "X" || "O"
-    true
-
+def position_taken?(index, board = [])
+  if board[index] == " "
+    false
+  
 
 
   end
